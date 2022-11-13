d56b800569a3f022574fffbb9576c9a9a067830c
Smail <44433524+Smail@users.noreply.github.com>
2022-11-13 14:22:07 +0100

    Render the to be sorted array as a bar chart

--- src/SortJPanel.java
+++ src/SortJPanel.java
@@ -3,8 +3,10 @@ import animated.sorts.NextSortStepListener;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Rectangle2D;
 
 public class SortJPanel extends JPanel implements NextSortStepListener {
+    private static final Color[] COLORS = new Color[]{Color.RED, Color.GREEN, Color.ORANGE, Color.MAGENTA};
     private AnimatedSortAlgorithm sorter;
 
     public SortJPanel() {
@@ -21,6 +23,44 @@ public class SortJPanel extends JPanel implements NextSortStepListener {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+
+        if (sorter == null || sorter.getArray() == null) return;
+
+        Graphics2D g2 = (Graphics2D) g.create();
+        double[] array = sorter.getArray();
+        double rectWidth = this.getWidth() / (double) array.length;
+
+        g2.setPaint(Color.WHITE);
+        g2.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
+
+        g2.drawString("Number of comparisons: " + sorter.getNumComparisons(), 1, this.getHeight() - 1);
+
+        for (int i = 0; i < array.length; i++) {
+            double x = i * rectWidth;
+            // Scale height to be between 1% and 95% of the panel's height.
+            double height = ((array[i] + 0.01) * 0.95) * this.getHeight();
+
+            if (sorter.isRunning()) {
+                int[] xs = sorter.getComparingElementIndices();
+
+                if (xs.length > COLORS.length) {
+                    System.out.println("WARNING: Not enough colors available. Need " + xs.length + " have: " + COLORS.length);
+                }
+
+                g2.setPaint(Color.WHITE);
+
+                for (int j = 0; j < Math.min(xs.length, COLORS.length); j++) {
+                    if (i == xs[j]) {
+                        g2.setPaint(COLORS[j]);
+                    }
+                }
+            } else {
+                // Color the sorted array green.
+                g2.setPaint(Color.GREEN);
+            }
+
+            g2.fill(new Rectangle2D.Double(x, 0, rectWidth + 1, height));
+        }
     }
 
     @Override
