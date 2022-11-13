574819a31451fd3a5c3dcf22c9b0845d521ec79f
Smail <44433524+Smail@users.noreply.github.com>
2022-11-13 14:26:37 +0100

    Implement selection sort

---
+++ src/animated/sorts/SelectionSortAnimated.java
@@ -0,0 +1,35 @@
+package animated.sorts;
+
+public class SelectionSortAnimated extends AnimatedSortAlgorithm {
+    private int unsortedPartitionStart, currentElement, minElement;
+
+    public SelectionSortAnimated(double[] array) {
+        super(array);
+    }
+
+    @Override
+    public void sortImpl() {
+        for (unsortedPartitionStart = 0; unsortedPartitionStart < array.length; unsortedPartitionStart++) {
+            minElement = unsortedPartitionStart;
+
+            // Find min
+            for (currentElement = unsortedPartitionStart + 1; currentElement < array.length; currentElement++) {
+                numComparisons++;
+
+                if (array[currentElement] < array[minElement]) {
+                    minElement = currentElement;
+                }
+
+                notifySortListeners();
+                suspendExecution();
+            }
+
+            swap(unsortedPartitionStart, minElement);
+        }
+    }
+
+    @Override
+    public int[] getComparingElementIndices() {
+        return new int[]{unsortedPartitionStart, currentElement, minElement};
+    }
+}
--- src/animated/sorts/SortAlgorithmFactory.java
+++ src/animated/sorts/SortAlgorithmFactory.java
@@ -5,10 +5,11 @@ public class SortAlgorithmFactory {
         return switch (algorithm) {
             case BUBBLE_SORT -> new BubbleSortAnimated(array);
             case INSERTION_SORT -> new InsertionSortAnimated(array);
+            case SELECTION_SORT -> new SelectionSortAnimated(array);
         };
     }
 
     public enum Algorithms {
-        BUBBLE_SORT, INSERTION_SORT
+        BUBBLE_SORT, INSERTION_SORT, SELECTION_SORT
     }
 }
