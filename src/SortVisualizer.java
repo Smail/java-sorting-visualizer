10208043fdd792c698f4b649d0aa349094f6b692
Smail <44433524+Smail@users.noreply.github.com>
2022-11-13 14:06:56 +0100

    Add SortJPanel to the JFrame

--- src/SortVisualizer.java
+++ src/SortVisualizer.java
@@ -1,10 +1,14 @@
 import javax.swing.*;
 
 public class SortVisualizer {
+    private final SortJPanel sortJPanel;
+
     public SortVisualizer() {
+        sortJPanel = new SortJPanel();
         JFrame frame = new JFrame("Sorting Visualizer");
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(sortJPanel, BorderLayout.CENTER);
         frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
