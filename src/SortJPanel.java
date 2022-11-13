459ac360401faf8f7b712c19148e5339e0dd24bd
Smail <44433524+Smail@users.noreply.github.com>
2022-11-13 14:05:55 +0100

    Add SortJPanel.java.
    It actually renders the visualization.

---
+++ src/SortJPanel.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class SortJPanel extends JPanel {
+    public SortJPanel() {
+        setPreferredSize(new Dimension(1280, 720));
+        setBackground(Color.BLACK);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+    }
+}
