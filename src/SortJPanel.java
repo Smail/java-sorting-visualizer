daed087de5f0fe25aa32694d90df54422343bcad
Smail <44433524+Smail@users.noreply.github.com>
2022-11-13 14:13:42 +0100

    Add sort method to SortJPanel

--- src/SortJPanel.java
+++ src/SortJPanel.java
@@ -1,12 +1,21 @@
+import animated.sorts.AnimatedSortAlgorithm;
 import animated.sorts.NextSortStepListener;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class SortJPanel extends JPanel implements NextSortStepListener {
+    private AnimatedSortAlgorithm sorter;
+
     public SortJPanel() {
         setPreferredSize(new Dimension(1280, 720));
         setBackground(Color.BLACK);
+    }
+
+    public void sort(AnimatedSortAlgorithm sorter) {
+        this.sorter = sorter;
+        sorter.addListener(this);
+        sorter.sort();
     }
 
     @Override
